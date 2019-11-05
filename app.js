f9fd6c64eb25868a577a46cb8af030060cab7f36
Gabor Simon <simon.gabor87hu@gmail.com>
2019-11-05 08:47:49 +0100

    test

---
+++ app.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const app = express();
+const exphbs  = require('express-handlebars');
+const path = require('path');
+
+//Static folder(path)
+app.use(express.static(path.join(__dirname, '/public')));
+
+//Handlebars middleware
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+//Routes
+//Render homepage
+app.get('/', function (req, res) {
+    res.render('home', {
+    })
+});
+
+//////Felvásárlás//////
+//Render Femkereskedelem
+app.get('/fem', function(req, res){
+    res.render('fem',{
+    })
+});
+//Render Elektronikai Felvasarlas
+app.get('/elektronikaifelv', function(req, res){
+    res.render('elektronikaifelv',{
+    })
+});
+
+//Render Papir Felvasarlas
+app.get('/papirfelv', function(req, res){
+    res.render('papirfelv',{
+    })
+});
+
+///////////Atvetel//////////
+//Render Elektronikai Atvetel
+app.get('/elektronikaiatv', function(req, res){
+    res.render('elektronikaiatv',{
+    })
+});
+
+//Render Papir Atvetel
+app.get('/papiratv', function(req, res){
+    res.render('papiratv',{
+    })
+});
+
+//Render Szolgaltatas
+app.get('/szolgaltatas', function(req, res){
+    res.render('szolgaltatas',{
+    })
+});
+
+//Render Kapcsolat
+app.get('/kapcsolat', function(req, res){
+    res.render('kapcsolat',{
+    })
+});
+
+const port = 5000;
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
