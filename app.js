47234bff0af15f54b0edc7468adfad3ab7fba2eb
Gábor Simon <simon.gabor87hu@gmail.com>
2021-01-08 17:53:53 +0100

    config

--- app.js
+++ app.js
@@ -81,7 +81,9 @@ app.get('/kapcsolat', function (req, res) {
     res.render('kapcsolat')
 });
 
-const port = process.env.PORT || 8080;
+// const port = process.env.PORT || 8080;
+const port = 80;
+
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
